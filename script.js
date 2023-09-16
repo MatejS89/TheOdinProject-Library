c85f848f0957439f40af04ff17e59985517d1d2f
Matej Sedlák <111086657+MatejS89@users.noreply.github.com>
2023-09-16 21:39:40 +0200

    Library: Add a function to create book cards

--- script.js
+++ script.js
@@ -11,16 +11,39 @@ function addBookToLibrary(author, title, pages, read) {
   myLibrary.push(new Book(author, title, pages, read));
 }
 
+function createCard(book) {
+  let card = document.createElement("li");
+  let title = document.createElement("p");
+  title.textContent = `"${book.title}"`;
+  card.appendChild(title);
+  let author = document.createElement("p");
+  author.textContent = `${book.author}`;
+  card.appendChild(author);
+  let pages = document.createElement("p");
+  pages.textContent = `${book.pages} pages`;
+  card.appendChild(pages);
+  let readStatus = document.createElement("button");
+  let readButtonPara = document.createElement("p");
+  readStatus.textContent = book.read ? "Read" : "Not read";
+  readStatus.classList.add(book.read ? "read" : "not-read");
+  readButtonPara.appendChild(readStatus);
+  card.appendChild(readButtonPara);
+  
+  let removeButtonPara = document.createElement("p");
+  let removeButton = document.createElement("button");
+  removeButton.textContent = "Remove";
+  removeButtonPara.appendChild(removeButton);
+  card.appendChild(removeButtonPara);
+  
+  card.classList.add("book-card");
+  console.log(title);
+  return card;
+}
+
 function displayBooks() {
   const display = document.querySelector(".book-list");
   myLibrary.forEach((book) => {
-    const item = document.createElement("li");
-    const node = document.createTextNode(
-      `${book.author}, ${book.title}, ${book.pages}, ${book.read}`
-    );
-    item.classList.add("book-card");
-    item.appendChild(node);
-    display.appendChild(item);
+    display.appendChild(createCard(book));
   });
 }
 
